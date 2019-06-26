f02cf1886acd569fba2bd6cc9d0571b96b6197b2
future_plant <hodde.j@gmail.com>
2019-06-26 15:36:01 +0200

    finalized preliminary timeline visualization of the infiltration map

--- infiltration.R
+++ infiltration.R
@@ -3,50 +3,90 @@ library(sp)
 library(FNN)
 library(rgdal)
 library(leaflet)
+library(dplyr)
+library(htmltools)
+library(htmlwidgets)
 
 
 # https://stackoverflow.com/questions/27782488/r-calculating-the-shortest-distance-between-two-point-layers
 
 hotels <- st_read('output/hotels.geoJSON')
-airbnb2019 <- st_read('data/products_airbnb/AirbnbPoints_20190408.geojson')
+#airbnb2019 <- st_read('data/products_airbnb/AirbnbPoints_20190408.geojson')
+airbnball <- st_read('data/products_airbnb/AirbnbPoints_Concat_2015to2019.geojson')
+unique(airbnball$date)
+
 nbr <- st_read('output/neighbourhoods.geojson')
 hotels <- st_intersection(hotels,nbr)
 
 hotels <- st_transform(hotels,28992)
 hotels <- as(hotels, 'Spatial')
 
-airbnb2019 <- st_transform(airbnb2019,28992)
-airbnb2019 <- as(airbnb2019, 'Spatial')
+airbnball <- st_transform(airbnball,28992)
+airbnball <- as(airbnball, 'Spatial')
  
-distances <- get.knnx(coordinates(hotels), coordinates(airbnb2019), k=1)
-airbnb2019 <- st_as_sf(airbnb2019)
-airbnb2019$dist2hot <- distances$nn.dist
-airbnb2019 <- st_transform(airbnb2019,4326)
+distances <- get.knnx(coordinates(hotels), coordinates(airbnball), k=1)
+airbnball <- st_as_sf(airbnball)
+airbnball$dist2hot <- distances$nn.dist
+airbnball <- st_transform(airbnball,4326)
 
-join <- st_join(airbnb2019, nbr)
-keeps <- c('id', 'Buurt_code', 'dist2hot')
+join <- st_join(airbnball, nbr)
+keeps <- c('id', 'Buurt_code', 'dist2hot','date')
 join <- join[keeps]
-test <- join[which(join$Buurt_code == 'M32a'),]
 
-st_write(join,'intermediate/airbnbdist.geojson', driver='GeoJSON')
+dates <- unique(join$date)
+nbrnames <- unique(nbr$Buurt_code)
 
-mean_distance <- aggregate(join, list(as.factor(join$Buurt_code)), FUN=mean)
-st_geometry(mean_distance) <- NULL
-mean_distance <- mean_distance[,-which(names(mean_distance) == "Buurt_code")]
-names(mean_distance) <- c("Buurt_code", "id", "dist2hot")
 
-nbr <- merge(nbr,mean_distance)
+distances <- data.frame(matrix(ncol = 0, nrow = 481),stringsAsFactors = F)
 
-bins <- c(0, 250, 500, 1000, 2500)
-pal <- colorBin("YlOrRd", domain = nbr$dist2hot, bins = bins)
+distances$Buurt_code <- nbrnames 
+startyear <- 2015
+for (date in dates){
+  subset <- join[which(join$date == date),]
+  mean_distance <- aggregate(subset, list(as.factor(subset$Buurt_code)), FUN=mean)
+  mean_distance <- mean_distance[,-which(names(mean_distance) == "Buurt_code")]
+  mean_distance <- mean_distance[,-which(names(mean_distance) == "id")]
+  mean_distance <- mean_distance[,-which(names(mean_distance) == "date")]
+  st_geometry(mean_distance) <- NULL
+  names(mean_distance) <- c("Buurt_code",paste0("dist2hot",startyear))
+  distances <- left_join(distances, mean_distance, by="Buurt_code")
+  startyear <- startyear + 1
+}
+
+nbr <- left_join(nbr,distances,by="Buurt_code")
+
+pal <- colorNumeric("YlOrRd", c(nbr$dist2hot2019,nbr$dist2hot2018,nbr$dist2hot2017,nbr$dist2hot2016,nbr$dist2hot2015),
+                    na.color = "transparent")
 
 m <- leaflet() %>% setView(lng = 4.898940, lat = 52.382676, zoom = 11)
 m %>% addProviderTiles(providers$OpenStreetMap.BlackAndWhite) %>%
-  addMarkers(data=hotels, clusterOptions = markerClusterOptions(), popup = ~hotelpopup) %>%
   addPolygons(data = nbr,color = "#444444", weight = 0.4, smoothFactor = 0.5,
               opacity = 1.0, fillOpacity = 0.3,
-              fillColor = ~pal(dist2hot),
+              fillColor = ~pal(dist2hot2015),
               highlightOptions = highlightOptions(color = "white", weight = 2,
-                                                  bringToFront = TRUE)) 
+                                                  bringToFront = TRUE), popup = ~airbnbpopup, group="2015") %>%
+  addPolygons(data = nbr,color = "#444444", weight = 0.4, smoothFactor = 0.5,
+              opacity = 1.0, fillOpacity = 0.3,
+              fillColor = ~pal(dist2hot2016),
+              highlightOptions = highlightOptions(color = "white", weight = 2,
+                                                  bringToFront = TRUE), popup = ~airbnbpopup,group="2016") %>%
+  addPolygons(data = nbr,color = "#444444", weight = 0.4, smoothFactor = 0.5,
+              opacity = 1.0, fillOpacity = 0.3,
+              fillColor = ~pal(dist2hot2017),
+              highlightOptions = highlightOptions(color = "white", weight = 2,
+                                                  bringToFront = TRUE), popup = ~airbnbpopup,group="2017") %>%
+  addPolygons(data = nbr,color = "#444444", weight = 0.4, smoothFactor = 0.5,
+              opacity = 1.0, fillOpacity = 0.3,
+              fillColor = ~pal(dist2hot2018),
+              highlightOptions = highlightOptions(color = "white", weight = 2,
+                                                  bringToFront = TRUE), popup = ~airbnbpopup,group="2018") %>%
+  addPolygons(data = nbr,color = "#444444", weight = 0.4, smoothFactor = 0.5,
+              opacity = 1.0, fillOpacity = 0.3,
+              fillColor = ~pal(dist2hot2019),
+              highlightOptions = highlightOptions(color = "white", weight = 2,
+                                                  bringToFront = TRUE), popup = ~airbnbpopup,group="2019") %>%
+  addLayersControl(
+    baseGroups = c("2015", "2016", "2017", "2018", "2019"),
+    options = layersControlOptions(collapsed = FALSE))
   
 
